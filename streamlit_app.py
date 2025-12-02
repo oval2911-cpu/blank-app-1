5e15443a6ee71abe83c1260c04c9e5169aed4239
oval2911-cpu <oval2911@gmail.com>
2025-12-02 09:51:31 +0000

    loading dataset

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
-import streamlit as st
+"""import streamlit as st
 
 st.title("🎈 My new app")
 st.write(
     "I now changed somethign"
-)
+)"""
+#import libraries
+import pandas as pd
+
+#load dataset
+data_heart = pd.read_csv('https://raw.githubusercontent.com/LUCE-Blockchain/Databases-for-teaching/refs/heads/main/Framingham%20Dataset.csv')
+
+#see the dataset
+data_heart.head()
